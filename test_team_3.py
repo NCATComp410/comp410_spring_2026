88a92fc7ceefbf9770798c807bb69429644edf4e
Makell M Williams <mmwilliams4@aggies.ncat.edu>
2026-02-13 21:01:34 +0000

    Added URL test cases for test_url

--- test_team_3.py
+++ test_team_3.py
@@ -9,8 +9,28 @@ class TestTeam__3(unittest.TestCase):
         """Test to make sure Aggie Pride is shown correctly"""
         self.assertEqual(show_aggie_pride(), "Aggie Pride - Worldwide")
 
-    def test_url(self):
+    def test_url(self): # By Makell M Williams, with help from the video and Google Gemini.
         """Test URL functionality"""
+        # Followed the video to hide the url parts in lists.
+        start = ['www', 'http', 'https'] # Typical beginning of a url.
+        middle = ['game', 'plane'] # Some basic domains, may or may not actually exists.
+        end = ['com', 'edu', '000'] # 2 normal top level domains and a fake one '000'
+
+        # Loop to begin testing all combos of the info above.
+        for s in start:
+            for m in middle:
+                for e in end:
+
+                    result = analyze_text(f"My url {s}.{m}.{e}", ['URL']) # My URL being analyzed.
+                    
+                    # Got a little bit of help from Google Gemini, since I didn't understand the result would return what should be an empty list.
+                    if e == '000': # My negative test cases, since '000' is an invalid top level domain.
+                        print(f"URL {s}.{m}.{e} is not valid!")
+                        self.assertEqual(len(result), 1)
+
+                    else: # My positive test cases, since everything else should be fine.
+                        print(f"URL {s}.{m}.{e} is valid!")
+                        self.assertTrue(result, f"URL {s}.{m}.{e}")
 
     def test_us_bank_number(self):
         """Test US_BANK_NUMBER functionality"""
