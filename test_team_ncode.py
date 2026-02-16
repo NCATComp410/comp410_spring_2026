6db61201d38f727c397de9df364d47e390d748df
Nii Oye Kpakpo <166158861+Alucardz18@users.noreply.github.com>
2026-02-16 17:49:57 +0000

    Added Credit_Card testcase

--- test_team_ncode.py
+++ test_team_ncode.py
@@ -12,6 +12,40 @@ class TestTeam_ncode(unittest.TestCase):
     def test_credit_card(self):
         """Test CREDIT_CARD functionality"""
 
+        # Positive test case
+        p1, p2, p3, p4 = "4111", "1111", "1111", "1111"
+        positive_text = f"My credit card number is {p1} {p2} {p3} {p4}."
+
+        # Analyze the text and check if the CREDIT_CARD entity is detected
+        pos_result = analyze_text(text=positive_text, entity_list=["CREDIT_CARD"])
+        self.assertTrue(pos_result, f"Expected to find a CREDIT_CARD entity in the text: {positive_text}")
+
+        # Check if the detected entity is of type CREDIT_CARD
+        self.assertEqual(pos_result[0].entity_type, "CREDIT_CARD")
+
+        # Check if the detected value matches the expected credit card number
+        matched_value = positive_text[pos_result[0].start:pos_result[0].end]
+        cleaned_value = matched_value.replace(" ", "").replace("-", "") 
+
+        # Check if the cleaned value consists of only digits and has a valid length for credit card numbers
+        self.assertTrue(cleaned_value.isdigit()) # Ensure the cleaned value contains only digits
+        self.assertGreaterEqual(len(cleaned_value), 13, f"Expected a valid credit card number with at least 13 digits, got: {cleaned_value}")
+        self.assertLessEqual(len(cleaned_value), 19, f"Expected a valid credit card number with at most 19 digits, got: {cleaned_value}")
+
+        # Check if the confidence score is above a reasonable threshold
+        self.assertGreaterEqual(pos_result[0].score,0.3)
+
+        # Negative test case
+        n1, n2, n3, n4 = "4111", "1111", "1111", "1112"  # Invalid credit card number
+        negative_text = f"My credit card number is {n1} {n2} {n3} {n4}."
+
+        # Analyze the negative text and ensure no CREDIT_CARD entity is detected
+        neg_result = analyze_text(text=negative_text, entity_list=["CREDIT_CARD"])
+        self.assertFalse(neg_result, f"Expected no CREDIT_CARD entity in the text: {negative_text}")
+
+
+
+
     def test_crypto(self):
         """Test CRYPTO functionality"""
 
