afa2a3535524375b09238d54c393a78381a563c0
Caveria <165973423+Caveria@users.noreply.github.com>
2026-02-16 20:34:43 +0000

    Added IT_FISCAL_CODE testcases

--- test_team_2.py
+++ test_team_2.py
@@ -14,6 +14,24 @@ class TestTeam__2(unittest.TestCase):
 
     def test_it_fiscal_code(self):
         """Test IT_FISCAL_CODE functionality"""
+        #IT_fiscal_code format
+        prefix = ['RSSMRA', 'BNCLRD']   # Surname Name
+        mid = ['85M01', '00H00'] # year month date
+        suffix = ['H501V', 'G482J'] # Place Control
+        # loop through all combinations
+        for p in prefix:
+            for m in mid:
+                for s in suffix:
+                    result = analyze_text(f'My IT_fiscal_code {p}{m}{s}', ['IT_FISCAL_CODE'])
+                    if m == '00H00':
+                        # negative test case - 00H00 is not valid
+                        self.assertFalse(result)
+                    else:
+                        #postive testcase
+                        self.assertTrue(result, f'Fiscal_code not Entity not recognized {p}{m}{s}')
+                        #[type: IT_FISCAL_CODE, start: 15, end: 31, score: 0.3] 
+                        self.assertEqual(result[0].entity_type, 'IT_FISCAL_CODE')
+                        self.assertAlmostEqual(result[0].score, 0.3, 2)
 
     def test_it_identity_card(self):
         """Test IT_IDENTITY_CARD functionality"""
