bbd1543ccc59571eddddfa9cbacee7365f41a554
Ciara McLaughlin <90720471+CiaraM100104@users.noreply.github.com>
2026-02-17 19:38:14 +0000

    Added IN_VOTER test cases

--- test_team_1.py
+++ test_team_1.py
@@ -23,6 +23,25 @@ class TestTeam__1(unittest.TestCase):
 
     def test_in_voter(self):
         """Test IN_VOTER functionality"""
+        # first 3 characters of IN_VOTER
+        prefix = ["AAA", "GHJ", "333", "3A4"] # first part of IN_VOTER
+        # last 7 digits of IN_VOTER
+        suffix = ["1234567", "2348764", "5703067","4570274"] # second part of IN_VOTER
+
+        # iterate through the prefix and suffix test cases
+        for p in prefix:
+            for s in suffix:
+
+                result = analyze_text(f"My IN_Voter is {p}{s}", ['IN_VOTER'])
+                
+                # negative test case, first 3 characters need to be alphabetical
+                if p == "333" or p == "3A4":
+                    self.assertFalse(result)
+                else:
+                     # positive test cases
+                    self.assertTrue(result,f"IN_VOTER not recognized{p}{s}")
+                    self.assertEqual(result[0].entity_type, "IN_VOTER")
+                    self.assertAlmostEqual(result[0].score, 0.75,2)
 
 
 if __name__ == '__main__':
