a88f72d4b14ab5486620992f044c2ab5745cc98d
oryange23-png <oryange23@gmail.com>
2026-02-17 10:23:42 -0500

    Update test_team_z.py

--- test_team_z.py
+++ test_team_z.py
@@ -49,7 +49,7 @@ class TestTeam__z(unittest.TestCase):
                     # positive cases
                     self.assertEqual(len(result), 1)
                     self.assertEqual(result[0].entity_type, "SG_UEN")
-                    self.assertAlmostEqual(result[0].score, 0.5)
+                    self.assertGreaterEqual(result[0].score, 0.5)
 
 
 
