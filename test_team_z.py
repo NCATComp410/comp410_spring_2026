b02871c331f8f804dc4695c3ad9d69fcaef78fb5
Mykala Tucker <mstucker1@aggies.ncat.edu>
2026-02-18 21:32:58 +0000

    Added Test cases IN_GSTIN

--- test_team_z.py
+++ test_team_z.py
@@ -3,7 +3,6 @@ import unittest
 from pii_scan import analyze_text, show_aggie_pride  # noqa 
 
 
-Hello Mykala
 class TestTeam__z(unittest.TestCase):
     """Test team _z PII functions"""
     def test_show_aggie_pride(self):
@@ -29,8 +28,26 @@ class TestTeam__z(unittest.TestCase):
 
     def test_in_gstin(self):
         """Test IN_GSTIN functionality"""
-        result = analyze_text('My IN_GSTIN is 12ABCDE345F6Z7', ['IN_GSTIN'])
-        print(result)
+        prefixes  = ["01", "09", "27", "37"]          # valid state codes
+        mids      = ["ABCDE1234F", "QWERT6789Y", "LMNOP4321A", "ZXCVB9876K"]  # PANs
+        suffix1s  = ["0", "2", "3", "4"]              # entity numbers
+        suffix2s  = ["D"]                             # must be Z
+        suffix3s  = ["5", "7", "9", "3"]              # checksums
+            
+        for prefix in prefixes:
+            for mid in mids:
+                for suffix1 in suffix1s:
+                    for suffix2 in suffix2s:
+                        for suffix3 in suffix3s:
+                            result = analyze_text(f"My IN_GSTIN is {prefix}{mid}{suffix1}{suffix2}{suffix3}", ["IN_GSTIN"])
+                            if suffix2 != 'Z':
+                                #negative test cause - has to be Z
+                                self.assertFalse(result)
+                            else:
+                                #positive test cause
+                                self.assertTrue(result, f'Entity not found {prefix}{mid}{suffix1}{suffix2}{suffix3}')
+                                self.assertEqual(result[0].entity_type, 'IN_GSTIN')
+                                self.assertAlmostEqual(result[0].score, 1.0, 2)
 
     def test_sg_nric_fin(self):
         """Test SG_NRIC_FIN functionality"""
