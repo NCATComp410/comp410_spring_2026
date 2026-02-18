56f109264599a23933713503b8ee5003e848025d
KendallDixonCS <kendalldixon747@gmail.com>
2026-02-18 12:24:02 +0000

    Added test cases to IT_Passport

--- test_team_2.py
+++ test_team_2.py
@@ -20,7 +20,19 @@ class TestTeam__2(unittest.TestCase):
 
     def test_it_passport(self):
         """Test IT_PASSPORT functionality"""
+        prefix = ['YA', 'AB']
+        numbers = ['1234567', '123456'] #7 Digits Valid, 6 Digits Invalid
 
+        for p in prefix:
+            for n in numbers:
+                result = analyze_text(f'MY IT_passport {p}{n}', ['IT_PASSPORT'])
+                if len(n) != 7:
+                    self.assertFalse(result)
+                else:
+                    self.assertTrue(result, f'Passport not recognized {p}{n}')
+                    self.assertEqual(result[0].entity_type, 'IT_PASSPORT')
+                    self.assertAlmostEqual(result[0].score,0.01,2)
+                    
     def test_it_vat_code(self):
         """Test IT_VAT_CODE functionality"""
 
